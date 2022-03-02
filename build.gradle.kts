991a30e99ba4c500597aa6017fd34bc9a4544f4c
Matheus Ramon de Moura Moraes <mramon@uolinc.com>
2022-03-02 13:50:05 -0300

    Mudando o Banco para o H2 e Usando FlyWay

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	runtimeOnly("mysql:mysql-connector-java")
+	implementation("org.flywaydb:flyway-core:8.5.1")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
