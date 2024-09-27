d6ac258c134a9baf34cdbd3fa1fcfa71c26b7956
Carlos A P Yamakawa <carlos.yamakawa@gmail.com>
2024-09-27 14:45:21 -0400

    erro 4 corrigido (#8)

--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -27,7 +27,7 @@ class BookService(
     }
 
     fun findById(id: Int): BookModel {
-        return bookRepository.findById(1).orElseThrow{NotFoundException(Errors.ML1001.message.format(id), Errors.ML1001.code)}
+        return bookRepository.findById(id).orElseThrow{NotFoundException(Errors.ML1001.message.format(id), Errors.ML1001.code)}
     }
 
     fun delete(id: Int) {
